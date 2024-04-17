e9496a51e9216341a27d66218535a078d02b189d
asom <86374869+as0mdude@users.noreply.github.com>
2024-04-17 16:37:16 -0700

    done

--- rules.js
+++ rules.js
@@ -1,32 +1,29 @@
 class Start extends Scene {
     create() {
         console.log(this.engine.storyData);
-
-        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title); 
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation);
     }
 }
 
 class Location extends Scene {
-    access_key = false; 
-    create(key) {
-        
-        
+ 
+    static hasAccessPass = false;
+ 
+    create(key) {       
         let locationData = this.engine.storyData.Locations[key];
-        this.engine.show(locationData.Body);
-        
-        if(locationData.Choices) { 
-            for(let choice of locationData.Choices) { 
+        this.engine.show(locationData.Body);        
+        if(locationData.Choices) {
+            for(let choice of locationData.Choices) {
                 this.engine.addChoice(choice.Text, choice.Target);
             }
         } else {
-            this.engine.addChoice("The end.")
+            this.engine.addChoice("The end.");
         }
-
         if(locationData.Interactions) {
             for(let action in locationData.Interactions) {
                 this.engine.addChoice(locationData.Interactions[action].Text, {action, location: key});
@@ -36,30 +33,26 @@ class Location extends Scene {
 
     handleChoice(choice) {
         if(choice) {
-            if (typeof choice == "object") {  
+            if (typeof choice == "object") {
                 let locationData = this.engine.storyData.Locations[choice.location];
                 this.engine.show(locationData.Interactions[choice.action].Result);
-
-                // when player chooses Access Pass, the Observation Deck is unlocked
                 if (choice.action === "Access Pass") {
-                    this.access_key = true; 
-                } 
-                if (choice) {
-                    this.engine.gotoScene(Location, this.engine.storyData.ReturnTo);
-                    this.engine.show("You need to purchase an access pass from the lounge, please go back to it.");
-                }else{
-                    this.engine.gotoScene(Location, choice.location);
+                    Location.hasAccessPass = true;
                 }
-                
-                
-            } 
-            
-            else if(choice) { 
-                
-                this.engine.show("&gt; "+choice);
-                this.engine.show("poop");
-                this.engine.gotoScene(Location, choice);
-
+                this.engine.gotoScene(Location, choice.location);
+            } else {
+                if (choice === "Observation Deck") {
+                    if (Location.hasAccessPass) {
+                        this.engine.show("&gt; "+choice);
+                        this.engine.gotoScene(Location, this.engine.storyData.AccessAccept);
+                    } else {
+                        this.engine.show("You need to purchase an access pass from the lounge, please go back to it.");
+                        this.engine.gotoScene(Location, this.engine.storyData.ReturnTo);
+                    }
+                } else {
+                    this.engine.show("&gt; "+choice);
+                    this.engine.gotoScene(Location, choice);
+                }
             }
         } else {
             this.engine.gotoScene(End);
@@ -68,14 +61,5 @@ class Location extends Scene {
 }
 
 
-
-
-
-class End extends Scene {
-    create() {
-        this.engine.show("<hr>");
-        this.engine.show(this.engine.storyData.Credits);
-    }
-}
 
 Engine.load(Start, 'myStory.json');
